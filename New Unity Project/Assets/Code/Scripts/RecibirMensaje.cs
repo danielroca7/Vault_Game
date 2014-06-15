19bd12421e38f79fe30fd76c1213ef8a08449c0c
danielroca7 <danielroca_7@hotmail.com>
2014-06-15 10:18:28 -0500

    Prefabs
    Aqui van los prefabs y los scribs con la utilidad del teclado

---
+++ New Unity Project/Assets/Code/Scripts/RecibirMensaje.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecibirMensaje : MonoBehaviour {
+
+	public GUIText textModule;
+
+	void Start ()
+	{
+		textModule = GetComponentInChildren<GUIText>();
+	}
+	public void EscribirEnModulo (string mensaje)
+	{
+		textModule.text = mensaje;
+	}
+}
